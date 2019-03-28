4dd539ff06bdcfa96eaf0a0069f2052de22e9fe5
Georgiana Paler <georgiana.paler@ibm.com>
2019-03-28 11:52:22 +0200

    guest page - create header and groups list

--- ionic-clientApp/src/pages/guest/guest.page.ts
+++ ionic-clientApp/src/pages/guest/guest.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IGROUP } from 'src/model/interfaces';
 
 @Component({
   selector: 'app-guest',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuestPage implements OnInit {
 
+
+  groupsList: Array<IGROUP> =[
+    {
+      id: 1,
+      name: "Wedding party"
+    },
+    {
+      id: 2,
+      name: "Bride's family"
+    },
+    {
+      id: 3,
+      name: "Groom's family"
+    },
+    {
+      id: 4,
+      name: "Mutual friends"
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  addNewGroup(){
+    const newGroup = {
+      id: this.groupsList.length+1,
+      name: "New group"
+    };
+
+    this.groupsList.push(newGroup);
   }
 
 }
