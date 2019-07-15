ec969b14715d15384e2050a8e5ceece0840af102
Georgiana Paler <georgiana.paler@ibm.com>
2019-07-15 22:01:53 +0300

    native storager

--- ionic-clientApp/src/app/app.module.ts
+++ ionic-clientApp/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouteReuseStrategy } from "@angular/router";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
-import { SplashScreen } from "@ionic-native/splash-screen/ngx";
-import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { Contacts, Contact } from "@ionic-native/contacts";
-import { ChartsModule } from "ng2-charts";
-import { NgCalendarModule } from "ionic2-calendar";
-import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { SharedModule } from "./pipes/shared.module";
-import { IonicStorageModule } from "@ionic/storage";
-import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Contacts, Contact } from '@ionic-native/contacts';
+import { ChartsModule } from 'ng2-charts';
+import { NgCalendarModule } from 'ionic2-calendar';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './pipes/shared.module';
+import { IonicStorageModule } from '@ionic/storage';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { PopoverComponent } from './components/popover/popover.component';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, "assets/i18n", ".json");
+  return new TranslateHttpLoader(http, 'assets/i18n', '.json');
 }
 
 @NgModule({
@@ -31,7 +32,7 @@ export function createTranslateLoader(http: HttpClient) {
     SharedModule,
     BrowserModule,
     IonicModule.forRoot({
-      mode: "ios",
+      mode: 'ios',
       scrollAssist: true
     }),
     AppRoutingModule,
@@ -53,6 +54,7 @@ export function createTranslateLoader(http: HttpClient) {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Contacts,
     Navigator,
+    NativeStorage,
     Contact
   ],
   bootstrap: [AppComponent],
--- ionic-clientApp/src/app/services/guests/guests.service.ts
+++ ionic-clientApp/src/app/services/guests/guests.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IGuest, IGROUP } from 'src/model/interfaces';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,37 @@ export class GuestsService {
 
   private guestsList: IGuest[] = [];
 
-  constructor() { }
+  constructor(
+    private nativeStorage: NativeStorage
+  ) { }
 
   deleteGuest(guest: IGuest) {
     const removeIndex = this.guestsList.map(function (item) { return item.id; }).indexOf(guest.id);
     // remove object
     this.guestsList.splice(removeIndex, 1);
+    this.nativeStorage.setItem('guests', this.guestsList)
+    .then(
+      () => console.log('Stored guests!'),
+      error => console.error('Error storing item', error)
+    );
   }
 
   addGuest(guest: IGuest) {
     this.guestsList.push(guest);
+    this.nativeStorage.setItem('guests', this.guestsList)
+    .then(
+      () => console.log('Stored guests!'),
+      error => console.error('Error storing item', error)
+    );
   }
 
   getAllGuest(): IGuest[] {
+    this.nativeStorage.getItem('guests').then(res => this.guestsList = res);
     return this.guestsList;
   }
 
   getGuestByGroup(group: IGROUP): IGuest[] {
+    this.nativeStorage.getItem('guests').then(res => this.guestsList = res);
     return this.guestsList.filter(
       guest => guest.group === group.name
     );
